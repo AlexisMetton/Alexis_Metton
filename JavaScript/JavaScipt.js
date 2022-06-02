e4960de09e36ad835364cc30cb1ffeed2b64d201
Alexis Metton <alexis.metton@gmail.com>
2022-06-02 12:05:34 +0200

    nhcg

--- JavaScript/JavaScipt.js
+++ JavaScript/JavaScipt.js
@@ -1,8 +1,6 @@
 function menu(){
     document.getElementById('burger').style.display = "none";
-    document.getElementById('croix').style.display = "block";    
-    document.getElementById('croix').style.rotate = "90";
-    document.getElementById('croix').style.transition = "250ms";
+    document.getElementById('croix').style.display = "block"; 
     document.getElementById('croix').style.zIndex = "3";
     document.getElementById('nav').style.display = "grid";
 }
@@ -10,7 +8,5 @@ function menu(){
 function menu1(){
     document.getElementById('burger').style.display = "block";
     document.getElementById('croix').style.display = "none";    
-    document.getElementById('croix').style.rotate = "180";
-    document.getElementById('croix').style.transition = "250ms";
     document.getElementById('nav').style.display = "none"; 
 }
