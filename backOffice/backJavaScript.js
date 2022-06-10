fad34cbcf22390ea235dae67ce4abcad4b66a999
Alexis Metton <alexis.metton@gmail.com>
2022-06-10 12:04:36 +0200

    chngbcfb

--- backOffice/backJavaScript.js
+++ backOffice/backJavaScript.js
@@ -20,6 +20,11 @@ function popupNouveau(){
     document.getElementById('popup1').style.transition = "500ms"; 
 }
 
+function formulaire(e){
+    let rest=e.target.id;
+
+    createCookie("nomProjet", rest, 10);
+}
 /********************************Ajax*****************************/
 function data(data) {
     let text = "";
@@ -63,29 +68,119 @@ function openModal(e){
 
   document.getElementById('popup').innerHTML ='';    
   document.getElementById('popup').innerHTML += 
-  `<img class="imageRestau1" id="imageRestau1" src="../img/`+ infos[0]['image1'] +`">
-  </img>
-<img class="imageRestau2" id="imageRestau2" src="../img/`+ infos[0]['image2'] +`">
-</img>
-<img class="imageRestau3" id="imageRestau3" src="../img/`+ infos[0]['image3'] +`">
-</img>
-<h3>`+ infos[0]['intitule'] +`</h3>
-<h1>`+ infos[0]['nom'] +`</h1>
-<p style="margin-bottom: 10px;">
-`+ infos[0]['description'] +`
-</p>
-<div class="utilisation" style="margin-bottom:10px">
-`+ infos[0]['outils'] +`
+`<form action="envoiModif.php" method="post">
+<h2 class="text-center" style="color:whitesmoke;margin-top:20px">Ajouter un projet</h2>       
+<div class="form-group">
+    <input type="number" name="Numéroduprojet" class="form-control" placeholder="*"  min="1" max="50" autocomplete="off" value="`+ infos[0]['id_Categorie'] +`" required>
 </div>
-<section class="portfolio-experiment">
-  <a href="`+ infos[0]['liens'] +`" target="_blank" style="text-decoration: none;">
-    <span class="text"><img src="../img/github.png" height="16px" style="margin-right: 5px;">Git Hub</span>
-    <span class="line -right"></span>
-    <span class="line -top"></span>
-    <span class="line -left"></span>
-    <span class="line -bottom"></span>
-  </a>
-</section>`
+<div class="form-group">
+    <input type="text" name="Nom" class="form-control" placeholder="Nom"  autocomplete="off" value="`+ infos[0]['nom'] +`" required>
+</div>
+<div class="form-group">
+    <input id="ImagesLiens" type="text" name="Image" class="form-control" placeholder="nomImage1.jpg"  autocomplete="off" value="`+ infos[0]['image1'] +`" required>
+    <input style="display:none;margin-left:10%;" id="ImagesChargement" type="file" name="Imagebis" class="form-control" placeholder="nomImage.jpg"  autocomplete="off" value="`+ infos[0]['image1'] +`">
+    <div onclick="chargementImage()" id="FileIm" class="file">File</div>
+    <div style="display:none" onclick="chargementLiens()" class="file" id="FileLi">Liens</div>
+</div>
+<div class="form-group">
+    <input id="ImagesLiens1" type="text" name="Image1" class="form-control" placeholder="nomImage2.jpg"  autocomplete="off" value="`+ infos[0]['image2'] +`" required>
+    <input style="display:none;margin-left:10%;" id="ImagesChargement1" type="file" name="Image1bis" class="form-control" placeholder="nomImage.jpg"  autocomplete="off" value="`+ infos[0]['image2'] +`">
+    <div onclick="chargementImage1()" id="FileIm1" class="file">File</div>
+    <div style="display:none" onclick="chargementLiens1()" class="file" id="FileLi1">Liens</div>
+</div>
+<div class="form-group">
+    <input id="ImagesLiens2" type="text" name="Image2" class="form-control" placeholder="nomImage3.jpg"  autocomplete="off" value="`+ infos[0]['image3'] +`" required>
+    <input style="display:none;margin-left:10%;" id="ImagesChargement2" type="file" name="Image2bis" class="form-control" placeholder="nomImage.jpg"  autocomplete="off" value="`+ infos[0]['image3'] +`">
+    <div onclick="chargementImage2()" id="FileIm2" class="file">File</div>
+    <div style="display:none" onclick="chargementLiens2()" class="file" id="FileLi2">Liens</div>
+</div>     
+<div class="form-group">
+    <input type="text" name="Intitulé" class="form-control" placeholder="Titre"  autocomplete="off" value="`+ infos[0]['intitule'] +`" required>
+</div>
+<div class="form-group">
+    <textarea cols="33" rows="15" name="Description" class="form-control" maxlength="100000" placeholder="Description"  autocomplete="off" required>`+ infos[0]['description'] +`</textarea>
+</div>
+<div class="form-group">
+    <textarea cols="33" rows="15" name="Outils" class="form-control" maxlength="100000" placeholder="Outils"  autocomplete="off" required>`+ infos[0]['outils'] +`</textarea>
+</div>
+<div class="form-group">
+    <input type="text" name="Liens" class="form-control" placeholder="Liens"  autocomplete="off" value="`+ infos[0]['liens'] +`" required>
+</div>
+<div class="form-group">
+    <section class="portfolio-experiment">
+    <a style="text-decoration: none;">
+        <span class="text"><button type="submit" name="envoi">Validez</button></span>
+        <span class="line -right"></span>
+        <span class="line -top"></span>
+        <span class="line -left"></span>
+        <span class="line -bottom"></span>
+    </a>
+    </section>
+</div>   
+</form>    `
 
   console.log(infos);
 });}
+
+function createCookie(name,value, days){
+    var expires;
+  
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+      
+    document.cookie = escape(name) + "=" + 
+        escape(value) + expires + "; path=/";
+}
+
+function chargementImage(){
+    createCookie("fileImage", "image", 10);
+    document.getElementById('ImagesLiens').style.display = "none";
+    document.getElementById('ImagesChargement').style.display = "block";
+    document.getElementById('FileIm').style.display = "none";
+    document.getElementById('FileLi').style.display = "block";
+}
+function chargementImage1(){
+    createCookie("fileImage1", "image1", 10);
+    document.getElementById('ImagesLiens1').style.display = "none";
+    document.getElementById('ImagesChargement1').style.display = "block";
+    document.getElementById('FileIm1').style.display = "none";
+    document.getElementById('FileLi1').style.display = "block";
+}
+function chargementImage2(){
+    createCookie("fileImage2", "image2", 10);
+    document.getElementById('ImagesLiens2').style.display = "none";
+    document.getElementById('ImagesChargement2').style.display = "block";
+    document.getElementById('FileIm2').style.display = "none";
+    document.getElementById('FileLi2').style.display = "block";
+}
+
+function chargementLiens(){
+    createCookie("fileImage", "image", -1);
+    document.getElementById('ImagesLiens').style.display = "block";
+    document.getElementById('ImagesLiens').style.marginLeft = "10%";
+    document.getElementById('ImagesChargement').style.display = "none";
+    document.getElementById('FileLi').style.display = "none";
+    document.getElementById('FileIm').style.display = "block";
+}
+function chargementLiens1(){
+    createCookie("fileImage1", "image1", -1);
+    document.getElementById('ImagesLiens1').style.display = "block";
+    document.getElementById('ImagesLiens1').style.marginLeft = "10%";
+    document.getElementById('ImagesChargement1').style.display = "none";
+    document.getElementById('FileLi1').style.display = "none";
+    document.getElementById('FileIm1').style.display = "block";
+}
+function chargementLiens2(){
+    createCookie("fileImage2", "image2", -1);
+    document.getElementById('ImagesLiens2').style.display = "block";
+    document.getElementById('ImagesLiens2').style.marginLeft = "10%";
+    document.getElementById('ImagesChargement2').style.display = "none";
+    document.getElementById('FileLi2').style.display = "none";
+    document.getElementById('FileIm2').style.display = "block";
+}
---
+++ backOffice/envoiModif.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+header("Content-Type: application.json");
+
+http_response_code(200);
+
+$servname = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "alexis_metton";
+$conditionForm = $_COOKIE['nomProjet'];
+
+
+try{
+    $conn = new PDO("mysql:host=$servname;dbname=$dbname;charset=utf8",$user, $pass);
+    
+}
+catch (PDOException $e){
+    echo "Erreur : " . $e->getMessage();
+}
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    function valid_donnees($donnees){
+        $donnees = trim($donnees);
+        $donnees = stripslashes($donnees);
+        $donnees = htmlspecialchars($donnees);
+        return $donnees;
+    }
+    
+    if(isset($_POST['envoi'])){
+        echo $conditionForm;
+        $id_Categorie_saisi = valid_donnees($_POST['Numéroduprojet']);
+        $nom_saisi = valid_donnees($_POST['Nom']);
+        $intitule_saisi = valid_donnees($_POST['Intitulé']);
+        $description_saisi = $_POST['Description'];
+        $outils_saisi = $_POST['Outils'];
+        $liens_saisi = $_POST['Liens'];
+        
+        if(isset($_COOKIE['fileImage'])){
+            $Image_saisi = valid_donnees($_POST['Imagebis']);
+        }else{
+            $Image_saisi = valid_donnees($_POST['Image']);
+        }
+        if(isset($_COOKIE['fileImage1'])){
+            $Image1_saisi = valid_donnees($_POST['Image1bis']);
+        }else{
+            $Image1_saisi = valid_donnees($_POST['Image1']);
+        }
+        if(isset($_COOKIE['fileImage2'])){
+            $Image2_saisi = valid_donnees($_POST['Image2bis']);
+        }else{
+            $Image2_saisi = valid_donnees($_POST['Image2']);
+        }
+        
+        $updateRestaurant = $conn->prepare('UPDATE projets SET id_Categorie = ?, nom = ?, image1=?, image2=?, image3=?, intitule=?, description=?, outils=?, liens=? WHERE nom = ?');
+        $updateRestaurant->execute(array($id_Categorie_saisi, $nom_saisi, $Image_saisi, $Image1_saisi, $Image2_saisi, $intitule_saisi, $description_saisi, $outils_saisi, $liens_saisi, $conditionForm));
+        $_SESSION['modif'] = 1;
+        header('location: backAcceuil.php');
+    }
